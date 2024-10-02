2d9b0d9d5ab6567fb26757c0b1bfd37b909abf18
DylanMG <dylan.glaser1@ucalgary.ca>
2024-10-02 14:16:24 -0700

    fix elpd weights

--- R/OOS_CV_summary.R
+++ R/OOS_CV_summary.R
@@ -83,12 +83,13 @@ m4b_LLs<- do.call(bind_cols,
   unname()
 
 # get elpds
-elpd_all <- as.data.frame(rbind(elpd(m1_LLs)$estimates,
-                                elpd(m2a_LLs)$estimates,
-                                elpd(m2b_LLs)$estimates,
-                                elpd(m3_LLs)$estimates,
-                                elpd(m4a_LLs)$estimates,
-                                elpd(m4b_LLs)$estimates))
+elpd_comp <- loo_compare(elpd(m1_LLs), elpd(m2a_LLs), elpd(m2b_LLs), elpd(m3_LLs), 
+                         elpd(m4a_LLs), elpd(m4b_LLs))
+
+elpd_comp_df <-  as.data.frame(elpd_comp) |>
+  mutate(Model = c("m4b", "m4a", "m2a", "m2b", "m3", "m1")) |>
+  select(Model, elpd_diff, se_diff)
+#^ merge this into table for reporting, new table 1 
 
 # build matrix to feed stacking_weights() - see help file for N*K matrix
 lpds <- matrix(NA, 993*5, 6)
@@ -110,26 +111,18 @@ st_weights_df <- stacking_weights(lpds_Z) |>
   mutate(Model = c("m1", "m2a", "m2b", "m3", "m4a", "m4b")) |>
   rename(Weight = 1)
 
-elpd_table <- elpd_all |>
-  mutate(Metric = rep(c("elpd", "IC"), 6), 
-         Model = c(rep("m1", 2), 
-                   rep("m2a", 2), 
-                   rep("m2b", 2), 
-                   rep("m3", 2), 
-                   rep("m4a", 2), 
-                   rep("m4b", 2))) |>
+elpd_weights_table <- elpd_comp_df |>
   left_join(st_weights_df, by = "Model") |>
-  select(Model, Metric, Estimate, SE, Weight) |>
   mutate(Weight = round(as.numeric(Weight), 4)) |>
   arrange(desc(Weight))
-rownames(elpd_table) <- NULL
 
-write.csv(elpd_table, here("output/CV/elpd_table.csv"))
+write.csv(elpd_weights_table, here("output/CV/elpd_weights_table.csv"))
 
 ### by population which performed best overall? ------------------------------------------
 pop_weights <- NULL #and weights
 pop_weights_hier <- NULL #weights between m2a and m4a to see w. and w/o prior
 pop_weights_quo <- NULL #weights between top model and status quo (m1)
+pop_weights_misc <- NULL
 
 j <- 1 #column tracker
 for(i in unique(SR_data$pop)){
@@ -172,6 +165,17 @@ for(i in unique(SR_data$pop)){
            Pop = i) |>
     rename(Weight = 1)
   pop_weights_hier <- rbind(pop_weights_hier, pop_weight_hier)
+  
+  #and whatever else just to check ---
+  lpds_Z_misc <- lpds[,c(1,4)] - apply(lpds[,c(1,4)],1,max)  #toggle cols around to compare other models
+  
+  pop_weight_misc <- stacking_weights(lpds_Z_misc[rows,]) |>
+    as.matrix() |>
+    as.data.frame()|>
+    mutate(Model = c("m1", "m3"), 
+           Pop = i) |>
+    rename(Weight = 1)
+  pop_weights_misc <- rbind(pop_weights_misc, pop_weight_misc)
   
   j <- j+n.rows #advance index to start of next pop 
 }
